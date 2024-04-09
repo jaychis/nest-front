c6e9f08342c87bfbbd1a74892a7fdacd3f23bce9
akdl911215 <akdl911215@naver.com>
2024-04-09 18:23:18 +0900

    global bar

--- src/pages/Global/GlobalBar.tsx
+++ src/pages/Global/GlobalBar.tsx
@@ -1,30 +1,34 @@
 import React from "react";
 import styles from './GlobalBar.module.css'
+import { FaSistrix, FaUserAlt, FaPlus } from 'react-icons/fa';
 
 const GlobalBar = () => {
 
-    return <>
-        <header className={styles.header}>
-            <div className={styles.contents}>
-                <div>
-                    로고 자리
-                </div>
+    return (
+        <nav style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            backgroundColor: '#FFFFFF',
+            padding: '10px'
+        }}>
+            {/* Logo */}
+            <img src="logo.png" alt="Logo" style={{width: '50px'}}/>
 
-                <nav>
-                    <ul>
-                        <li>
-                            메뉴 1
-                        </li>
-                        <li>
-                            메뉴 2
-                        </li>
-                    </ul>
-                </nav>
+            {/* Search Bar */}
+            <div style={{flexGrow: 1, marginLeft: '20px', marginRight: '20px'}}>
+                <input type="search" placeholder="Search Reddit" style={{width: '100%', padding: '10px'}}/>
             </div>
 
-
-        </header>
-    </>
+            {/* Navigation Icons */}
+            <div style={{display: 'flex', alignItems: 'center'}}>
+                <FaSistrix style={{marginRight: '20px'}}/> {/* Search Icon */}
+                <FaUserAlt style={{marginRight: '20px'}}/> {/* User Icon */}
+                <button style={{background: 'none', border: 'none'}}>
+                    <FaPlus/> {/* Plus/Create Icon */}
+                </button>
+            </div>
+        </nav>
+    );
 }
-
 export default GlobalBar
