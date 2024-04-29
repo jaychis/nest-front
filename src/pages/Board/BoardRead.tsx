d29b17322fc1d85095c818ce2aefbc4e4109c222
akdl911215 <akdl911215@naver.com>
2024-04-29 16:53:10 +0900

    comments && replies function,css add

--- src/pages/Board/BoardRead.tsx
+++ src/pages/Board/BoardRead.tsx
@@ -3,7 +3,7 @@ import GlobalBar from "../Global/GlobalBar";
 import GlobalSideBar from "../Global/GlobalSideBar";
 import Card from "../../components/Card";
 import { ReadAPI } from "../api/BoardApi";
-import { useLocation, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 interface CardType {
   readonly id: string;
@@ -12,6 +12,7 @@ interface CardType {
   readonly content: string;
   readonly title: string;
   readonly nickname: string;
+  readonly comments: CommentType[];
   readonly created_at: Date;
   readonly updated_at: Date;
   readonly deleted_at?: Date | null;
@@ -23,11 +24,110 @@ interface CommentType {
   readonly content: string;
   readonly nickname: string;
   readonly category: string;
-  readonly replies: [];
+  readonly replies: ReplyType[];
   readonly created_at: Date;
   readonly updated_at: Date;
   readonly deleted_at: null | Date;
 }
+
+interface ReplyType {
+  readonly id: string;
+  readonly comment_id: string;
+  readonly content: string;
+  readonly nickname: string;
+  readonly created_at: Date;
+  readonly updated_at: Date;
+  readonly deleted_at: null;
+}
+
+const Reply = (re: ReplyType) => {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          // alignItems: "center",
+          fontFamily: "Arial, sans-serif",
+          marginBottom: "10px",
+        }}
+      >
+        <div style={{ display: "flex" }}>
+          <div style={{ marginRight: "18px" }}>
+            <img
+              src={""}
+              alt={`${re.nickname}'s avatar`}
+              style={{
+                width: "40px",
+                height: "40px",
+                borderRadius: "50%",
+              }}
+            />
+          </div>
+          <div style={{ fontWeight: "bold", color: "#333" }}>{re.nickname}</div>
+        </div>
+        <div
+          style={{
+            backgroundColor: isHovered ? "#f0f0f0" : "white",
+            borderRadius: "10px",
+            padding: "8px",
+            width: "100%",
+          }}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <div style={{ marginTop: "4px" }}>{re.content}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const Comment = (co: CommentType) => {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          fontFamily: "Arial, sans-serif",
+          marginBottom: "10px",
+        }}
+      >
+        <div style={{ display: "flex" }}>
+          <div style={{ marginRight: "18px" }}>
+            <img
+              src={""}
+              alt={`${co.nickname}'s avatar`}
+              style={{
+                width: "40px",
+                height: "40px",
+                borderRadius: "50%",
+              }}
+            />
+          </div>
+          <div style={{ fontWeight: "bold", color: "#333" }}>{co.nickname}</div>
+        </div>
+        <div
+          style={{
+            backgroundColor: isHovered ? "#f0f0f0" : "white",
+            borderRadius: "10px",
+            padding: "8px",
+            width: "100%",
+          }}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <div style={{ marginTop: "4px" }}>{co.content}</div>
+        </div>
+      </div>
+    </>
+  );
+};
 const BoardRead = () => {
   const [params, setParams] = useSearchParams();
   const [board, setBoard] = useState<CardType>({
@@ -37,17 +137,7 @@ const BoardRead = () => {
     content: "",
     title: "",
     nickname: "",
-    created_at: new Date(),
-    updated_at: new Date(),
-    deleted_at: null,
-  });
-  const [comment, setComment] = useState<CommentType>({
-    id: "",
-    board_id: "",
-    content: "",
-    nickname: "",
-    category: "",
-    replies: [],
+    comments: [],
     created_at: new Date(),
     updated_at: new Date(),
     deleted_at: null,
@@ -76,15 +166,19 @@ const BoardRead = () => {
         {comments.map((co) => {
           return (
             <>
-              <div>
-                <div>
-                  <img alt={`${co.nickname}'s avatar`} />
-                </div>
-                <div>
-                  <div>{co.nickname}</div>
-                  {/*<div>{co.updated_at}</div>*/}
-                  <div>{co.content}</div>
-                </div>
+              <Comment
+                id={co.id}
+                board_id={co.board_id}
+                content={co.content}
+                nickname={co.nickname}
+                category={co.category}
+                replies={co.replies}
+                created_at={co.created_at}
+                updated_at={co.updated_at}
+                deleted_at={co.deleted_at}
+              />
+              <div style={{ marginLeft: "40px" }}>
+                {co.replies.length > 0 ? renderReplies(co.replies) : null}
               </div>
             </>
           );
@@ -92,7 +186,23 @@ const BoardRead = () => {
       </div>
     );
   };
-
+  const renderReplies = (replies: ReplyType[]) => {
+    return (
+      <div>
+        {replies.map((re) => (
+          <Reply
+            id={re.id}
+            comment_id={re.comment_id}
+            content={re.content}
+            nickname={re.nickname}
+            created_at={re.created_at}
+            updated_at={re.updated_at}
+            deleted_at={re.deleted_at}
+          />
+        ))}
+      </div>
+    );
+  };
   return (
     <>
       <GlobalBar />
@@ -107,6 +217,7 @@ const BoardRead = () => {
             createdAt={board.created_at}
             content={board.content}
           />
+          {board.comments?.length > 0 ? renderComments(board.comments) : null}
         </div>
       </div>
     </>
