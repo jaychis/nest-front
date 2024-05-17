cf74c1d86b13bda284adfaba27a9602f43ed49c3
akdl911215 <akdl911215@naver.com>
2024-05-17 15:14:52 +0900

    Task if Transforming the State of the Main List

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -2,11 +2,13 @@ import { combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import profileReducer from "./profileSlice";
 import userModalReducer from "./userModalSlice";
+import sideBarButtonReducer from "./mainListTypeSlice";
 
 const rootReducer = combineReducers({
   user: userReducer,
   profile: profileReducer,
   userModal: userModalReducer,
+  sideBarButton: sideBarButtonReducer,
 });
 
 export default rootReducer;
