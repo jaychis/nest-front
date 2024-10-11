a3829f2e195041eecc595fd82fa1ae52ca5438b3
akdl911215 <akdl911215@naver.com>
2024-10-11 10:23:15 +0900

    delete: FAQ functionality and html/css

--- src/pages/FrequentlyAskedQuetions/FrequentlyAskedQuestionsList.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FrequentlyAskedQuestionsList = () => {
-  return <>FAQ LIST</>;
-};
-
-export default FrequentlyAskedQuestionsList;
--- src/pages/FrequentlyAskedQuetions/FrequentlyAskedQuestionsRead.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const FrequentlyAskedQuestionsRead = () => {
-  return <>FAQRead</>;
-};
-export default FrequentlyAskedQuestionsRead;
