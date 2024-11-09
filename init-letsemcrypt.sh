8c2ba35a7bd9deb7ce4e4879f4be58656ebda0f2
akdl911215 <akdl911215@naver.com>
2024-11-09 16:45:19 +0900

    modify: https apply

---
+++ init-letsemcrypt.sh
@@ -0,0 +1,16 @@
+##!/bin/bash
+#
+#domains=(jaychis.com www.jaychis.com conf.d.jaychis.com)
+#email="akdl911215@naver.com" # 이메일 주소를 입력하세요.
+#staging=0 # 테스트 중에는 1로 설정하세요. 발급 중에는 0으로 설정합니다.
+#
+#if [ -d "./nginx/certificates/live/${domains[0]}" ]; then
+#  echo "이미 인증서가 존재합니다."
+#else
+#  echo "인증서를 발급합니다."
+#  docker-compose run --rm certbot certonly --webroot -w /etc/letsencrypt/www \
+#    -d "${domains[@]}" \
+#    --email "$email" \
+#    --agree-tos \
+#    --no-eff-email
+#fi
--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,8 +7,8 @@ fi
 
 domains=("jaychis.com" "www.jaychis.com"  "prod.jaychis.com")
 rsa_key_size=4096
-data_path="./data/certbot"
-email="akdl911215@naver.comD" # Adding a valid address is strongly recommended
+data_path="./nginx/certificates"
+email="akdl911215@naver.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
