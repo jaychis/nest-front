426f72f852e43a722e66cb7b3479c5b5432bd749
akdl911215 <akdl911215@naver.com>
2024-12-26 17:23:43 +0900

    refacto: community banner

--- src/pages/Board/CommunityBanner.tsx
+++ src/pages/Board/CommunityBanner.tsx
@@ -3,9 +3,7 @@ import CommunityProfile from './CommunityProfile';
 import { SelectCommunityParams } from '../../reducers/communitySlice';
 import { useSelector } from 'react-redux';
 import { breakpoints } from '../../_common/breakpoint';
-
-const URL_LOGO: string =
-  'https://img.freepik.com/free-photo/gray-wall-textures-background_74190-4389.jpg?t=st=1735131572~exp=1735135172~hmac=ee4001c10e2f66316b1d4e9f74a5c1b36659527d9132abca7d98892b42d85cf0&w=1380';
+import BANNER from '../../assets/img/community_banner.png';
 
 const CommunityBanner = () => {
   const selectCommunity: SelectCommunityParams = useSelector(
@@ -16,7 +14,7 @@ const CommunityBanner = () => {
     <>
       <BackgroundContainer>
         <BackgroundImage
-          src={selectCommunity?.banner || URL_LOGO}
+          src={selectCommunity?.banner || BANNER}
           alt="Description"
         />
         <CommunityProfile />
