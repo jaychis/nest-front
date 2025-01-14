917cc3000fb0e44ba9108ff625a7038551d9771b
akdl911215 <akdl911215@naver.com>
2025-01-14 15:39:55 +0900

    feat: shorts

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -22,23 +22,54 @@ import styled from 'styled-components';
 import ShareComponent from './ShareComponent';
 import { breakpoints } from '../_common/breakpoint';
 import { handleReaction } from '../_common/handleUserReaction';
-import {
-  fetchProfileImage,
-  FetchProfileImageType,
-} from '../_common/fetchCardProfile';
 import Carousel from './Carousel';
 
-const getYouTubeVideoId = ({ url }: { readonly url: string }): string => {
+const getVideoIdFromUrl = ({
+  url,
+}: {
+  readonly url: string;
+}): { readonly platform: string; readonly id: string } => {
   try {
-    return url.includes('v=')
-      ? url?.split('v=')[1]?.split('&')[0]
-      : url?.split('youtu.be/')[1]?.split('?')[0];
+    if (url.includes('youtube.com/watch') && url.includes('v=')) {
+      return { platform: 'youtube', id: url.split('v=')[1]?.split('&')[0] };
+    } else if (url.includes('youtu.be/')) {
+      return {
+        platform: 'youtube',
+        id: url.split('youtu.be/')[1]?.split('?')[0],
+      };
+    } else if (url.includes('youtube.com/shorts/')) {
+      return {
+        platform: 'youtube_shorts',
+        id: url.split('shorts/')[1]?.split('?')[0],
+      };
+    } else if (url.includes('instagram.com/reel/')) {
+      return {
+        platform: 'instagram',
+        id: url.split('reel/')[1]?.split('/')[0],
+      };
+    } else if (url.includes('facebook.com/share/v/')) {
+      return {
+        platform: 'facebook_video',
+        id: url.split('/v/')[1]?.split('/')[0],
+      };
+    } else if (url.includes('facebook.com/share/r/')) {
+      return {
+        platform: 'facebook_reel',
+        id: url.split('/r/')[1]?.split('/')[0],
+      };
+    } else if (url.includes('threads.net/')) {
+      return { platform: 'threads', id: url.split('/post/')[1]?.split('?')[0] };
+    } else if (url.includes('reddit.com/')) {
+      return { platform: 'reddit', id: url.split('/s/')[1]?.split('?')[0] };
+    } else {
+      console.warn('Unsupported URL format:', url);
+      return { platform: 'unknown', id: '' };
+    }
   } catch (e) {
-    console.error('Invalid URL', e);
-    return '';
+    console.error('Error extracting video ID:', e);
+    return { platform: 'error', id: '' };
   }
 };
-
 const Card = ({
   id,
   category,
@@ -314,21 +345,65 @@ const Card = ({
             </MediaContainer>
           ) : (
             <>
-              {content.map((video: string, index: number) => (
-                <VideoContainer key={`${id}-${index}`}>
-                  {video && (
-                    <YouTube
-                      videoId={getYouTubeVideoId({ url: video })}
-                      opts={{
-                        width: '100%',
-                        height: '400px',
-                        playerVars: { modestbranding: 1 },
-                      }}
-                      style={{ borderRadius: '20px' }} // 추가된 부분
-                    />
-                  )}
-                </VideoContainer>
-              ))}
+              {content.map((video: string, index: number) => {
+                const { platform, id } = getVideoIdFromUrl({ url: video });
+                const isYoutube = platform === 'youtube';
+                const videoHeight = isYoutube ? '400px' : '1000px';
+
+                return (
+                  <VideoContainer key={`${id}-${index}`}>
+                    {(platform === 'youtube' ||
+                      platform === 'youtube_shorts') &&
+                      id && (
+                        <YouTube
+                          videoId={id}
+                          opts={{
+                            width: '100%',
+                            height: videoHeight,
+                            playerVars: { modestbranding: 1 },
+                          }}
+                          style={{ borderRadius: '20px' }}
+                        />
+                      )}
+                    {platform === 'instagram' && id && (
+                      <iframe
+                        src={`https://www.instagram.com/reel/${id}/embed`}
+                        width="100%"
+                        height={videoHeight}
+                        style={{ borderRadius: '20px', border: 'none' }}
+                        allowFullScreen
+                      ></iframe>
+                    )}
+                    {platform === 'facebook_video' && id && (
+                      <iframe
+                        src={`https://www.facebook.com/video/embed?video_id=${id}`}
+                        width="100%"
+                        height={videoHeight}
+                        style={{ borderRadius: '20px', border: 'none' }}
+                        allowFullScreen
+                      ></iframe>
+                    )}
+                    {platform === 'reddit' && id && (
+                      <iframe
+                        src={`https://www.reddit.com/embed/${id}`}
+                        width="100%"
+                        height={videoHeight}
+                        style={{ borderRadius: '20px', border: 'none' }}
+                        allowFullScreen
+                      ></iframe>
+                    )}
+                    {platform === 'threads' && id && (
+                      <iframe
+                        src={`https://www.threads.net/embed/post/${id}`}
+                        width="100%"
+                        height={videoHeight}
+                        style={{ borderRadius: '20px', border: 'none' }}
+                        allowFullScreen
+                      ></iframe>
+                    )}
+                  </VideoContainer>
+                );
+              })}
             </>
           )}
         </ContentContainer>
