927ed544fe77ad9f87a73a4108ecccd3900e8c00
jungpill <ljp10777@gmail.com>
2025-02-18 21:01:38 +0900

    로그인 테스트 케이스 추가

--- src/components/tests/Login.test.tsx
+++ src/components/tests/Login.test.tsx
@@ -30,7 +30,6 @@ describe('로그인 테스트', () => {
         const emailInput = screen.getByTestId('email-input');
         const submitButton = screen.getByTestId('submit-button');
     
-        // act로 감싸서 상태 업데이트를 기다림
         await React.act(async () => {
           await user.type(emailInput, 'wjnaver.com');
           await user.click(submitButton);
@@ -41,4 +40,38 @@ describe('로그인 테스트', () => {
           expect(errorText).toHaveTextContent('유효한 이메일 주소를 입력하세요');
         });
     });
+
+    test('ID혹은 비밀번호가 올바르지 않을때', async() => {
+        const emailInput = screen.getByTestId('email-input')
+        const passwordInput = screen.getByTestId('password-input')
+        const submitButton = screen.getByTestId('submit-button');
+
+        await React.act(async () => {
+            await user.type(emailInput, 'tka22@test8.com')
+            await user.type(passwordInput, 'testtest8!')
+            await user.click(submitButton)
+        })
+
+        await waitFor(() => {
+            const errorText = screen.getByTestId('error-text');
+            expect(errorText).toHaveTextContent('로그인 실패. 이메일과 비밀번호를 확인하세요.');
+        });
+    })
+
+    test('비밀번호 형식이 잘못 됐을때', async() => {
+        const passwordInput = screen.getByTestId('password-input')
+        const submitButton = screen.getByTestId('submit-button');
+        const emailInput = screen.getByTestId('email-input');
+
+        await React.act(async() => {
+            await user.type(emailInput, 'wjdvlf99@naver.com')
+            await user.type(passwordInput, 'testtest')
+            await user.click(submitButton)  
+        })
+
+        await waitFor(() => {
+            const errorText = screen.getByTestId('error-text');
+            expect(errorText).toHaveTextContent('비밀번호는 최소 8자, 하나 이상의 문자, 하나의 숫자 및 하나의 특수문자입니다.');
+        })
+    })
 });
